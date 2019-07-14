71270e81a248c47c48207f5e6562117e11ff15e4
Ayman <aymalsh@gmail.com>
2019-07-14 06:20:34 +0300

    Step 3: Setup GCE

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger.backend;
 
+import com.aymanshehri.jokes.JokeTeller;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -19,10 +20,12 @@ import javax.inject.Named;
 public class MyEndpoint {
 
     /** A simple endpoint method that takes a name and says Hi back */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    @ApiMethod(name = "tellJoke")
+    public MyBean tellJoke() {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+
+        JokeTeller jokeTeller = new JokeTeller();
+        response.setData(jokeTeller.getRandomJoke());
 
         return response;
     }
